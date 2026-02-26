2bbad97a41ab9258f9c0bdc6bc9e6c8ba6fa7779
Jakub Štěpánek <jakub14step@seznam.cz>
2026-02-26 19:35:38 +0100

    testování v sveltekitu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,28 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+
+
+<main>
+
+<div id="a">
+	<p>awdawdawdasd</p>
+</div>
+
+
 {@render children()}
+</main>
+<style>
+	#a{
+		background-color: yellow;
+		width: 100px;
+		height: 20px;
+	}
+	main{
+		background-color: brown;
+	}
+	:global(body){
+		background-color: aqua;
+	}
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Meteostanica</h1>
+<p>Lorem <a href="/navody">ipsum</a> dolor sit amet, consectetur adipisicing elit. Hic eligendi ut a rerum tempora aut harum et maiores, voluptatem quisquam, consectetur ipsum eos cumque ab sed, illo omnis quas minima.</p>
+<style>
+    h1{
+        color:red;
+    }
+</style>
